b544e108551ebdfaa560f3561a7e9d39163b0f4b
Alex Liang <alex.c.liang@gmail.com>
2018-11-26 02:00:02 +0000

    fix: release

--- release.config.js
+++ release.config.js
@@ -1,11 +1,13 @@
-module.export = {
+module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
-      '@semantic-release/github',
+      '@semantic-release/git',
       {
-        assets: [{ path: 'dist/**/*.*', label: 'CSS distribution' }],
+        assets: ['dist/**/*.*', 'docs', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
